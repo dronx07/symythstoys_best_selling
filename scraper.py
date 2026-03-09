aa4f4862e3b2510ea5ddcf9a9ed0d50f22b0cd9f
DRONX <dronxx07@gmail.com>
2026-03-09 13:08:00 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 class ProductRunner:
-    def __init__(self, category_urls, headless=True, min_delay=2, max_delay=5):
+    def __init__(self, category_urls, headless=False, min_delay=2, max_delay=5):
         self.category_urls = category_urls
         self.homepage = "https://www.smythstoys.com/fr/fr-fr/"
         self.headless = headless
